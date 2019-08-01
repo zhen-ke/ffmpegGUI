3c80fa26fd32e5f0550224b05f9a1413a0193c8f
zhen-ke <cool420984@gmail.com>
2019-08-01 18:13:09 +0800

    添加禁用安全警告

--- src/main/index.js
+++ src/main/index.js
@@ -44,6 +44,10 @@ const template = [
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
+
+// 禁用安全警告
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
 if (process.env.NODE_ENV !== "development") {
   global.__static = require("path")
     .join(__dirname, "/static")
