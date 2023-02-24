6788c9ec93b472567cbfef28cd5edea205cc0f6d
zhen-ke <xmit0806@outlook.com>
2023-02-24 16:30:20 +0800

    fix: 修复progressbar展示不准的问题

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,4 +1,5 @@
 import { shell, dialog } from "@tauri-apps/api";
+import { type } from "@testing-library/user-event/dist/type";
 const { Command } = shell;
 export const { open, message } = dialog;
 
@@ -38,12 +39,17 @@ export const runFFmpeg = async (command, outputFolder, onProgress) => {
   console.log(child);
 };
 
+let duration = "";
+
 // 字符转对象
 const parseProgressLine = (line) => {
   var progress = {};
-
+  if (line.startsWith("  Duration")) {
+    duration = line.split(",")[0].replace(/:\s+/g, "=");
+  }
   // Remove all spaces after = and trim
-  line = line.replace(/=\s+/g, "=").trim();
+  line = duration.trim() + " " + line.replace(/=\s+/g, "=").trim();
+  // console.log(line.split(" "), "-----------------");
   var progressParts = line.split(" ");
 
   // Split every progress part by "=" to get key and value
@@ -61,13 +67,29 @@ const parseProgressLine = (line) => {
   return progress;
 };
 
+// hh:mm:ss to ss
+const getSeconds = (hms) => {
+  if (typeof hms !== "string") {
+    return 0;
+  }
+  const [h, m, s] = hms.split(":");
+  return +h * 60 * 60 + +m * 60 + +s;
+};
+
 // 获取进度
 const getProgress = (line) => {
-  const { frame } = parseProgressLine(line.toString()) || {};
-  if (frame) {
-    const progress = (previousFram / +frame) * 100;
-    previousFram = frame;
-    return progress;
+  const { time, Duration } = parseProgressLine(line.toString()) || {};
+
+  if (time && Duration) {
+    const t = getSeconds(time) / getSeconds(Duration);
+    return (100 * t).toFixed(2);
   }
   return 0;
+};
+
+const progress = (e) => {
+  var t = e.ratio;
+  const percentage = "Progress percentage: " + (100 * t).toFixed(2) + "%";
+  console.log(percentage, "percentage");
+  return percentage;
 };
