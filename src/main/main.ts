a1594c7dace2fe12aa8d504e4ccdc21f3913b3d3
zhen-ke <cool420984@gmail.com>
2024-10-15 19:54:22 +0800

    窗口添加最小高度和最小宽度

--- src/main/main.ts
+++ src/main/main.ts
@@ -572,6 +572,8 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
+    minWidth: 1024,
+    minHeight: 728,
     icon: getAssetPath('icon.png'),
     frame: true,
     titleBarStyle: 'hidden', // 隐藏  标题栏，但保留交通灯按钮
